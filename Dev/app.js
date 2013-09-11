e0fe615dfc3f7a529c86dda3f875e856fe9e77fc
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-11 12:33:33 -0700

    remove test code

--- Dev/app.js
+++ Dev/app.js
@@ -1,23 +1,10 @@
+//dependencies 
 var express = require('express');
 var mongoose = require('mongoose');
 var fs = require('fs');
 var config = require('./config/config');
-var request = require('request');
-var querystring = require('querystring');
+
 var app = express();
-
-// // var data = {
-// //   url: 'https://www.google.com/images/srpr/logo4w.png', 
-// //   content_type: 'image/png'
-// // };
-
-// // request.post({
-// //   url: 'http://localhost:3000/image',
-// //   headers:{'content-type': 'application/x-www-form-urlencoded'},
-// //   body: querystring.stringify(data) 
-// // }, function(err, response, body) {
-// //   console.log(body);
-// // });
 
 //models
 var modelsPath = __dirname + '/app/models';
@@ -34,7 +21,7 @@ require('./config/express')(app, config);
 require('./config/routes')(app);
 
 //database
-require('./config/db')(app, config);
+//require('./config/db')(app, config);
 
 //start app
 app.listen(config.port);
