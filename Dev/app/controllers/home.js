6a587bac679a78ed2f53c41308c3253b94b46300
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-10 22:31:28 -0700

    add and render jade files

---
+++ Dev/app/controllers/home.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'),
+  Article = mongoose.model('Article');
+
+exports.index = function(req, res){
+  Article.find(function(err, articles){
+    if(err) throw new Error(err);
+    res.render('home/index', {
+      title: 'Generator-Express MVC',
+      articles: articles
+    });
+  });
+};
