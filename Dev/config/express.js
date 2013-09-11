94feddae977b3272a67e297eb9835003ecb6a938
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-10 22:15:44 -0700

    reconfig to render view

--- Dev/config/express.js
+++ Dev/config/express.js
@@ -5,6 +5,9 @@ module.exports = function(app, config) {
     app.use(express.compress());
     app.use(express.static(config.root + '/public'));
     app.set('port', config.port);
+    app.set('views', config.root + '/app/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon(config.root + '/public/img/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
--- Dev/config/routes.js
+++ Dev/config/routes.js
@@ -1,4 +1,9 @@
 module.exports = function(app) {
+  //home route
+  var home = require('../app/controllers/home');
+  app.get('/', home.index);
+
+  //image route  
   var imageDataController = require('../app/controllers/ImageData.js');
   app.post('/image', imageDataController.storeImageMetaData);
 };
