f4d75c595680b988db437e936ab77d7e06942133
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-10 22:16:09 -0700

    save image data

--- Dev/app/controllers/ImageData.js
+++ Dev/app/controllers/ImageData.js
@@ -4,6 +4,14 @@ var ImageMetaData = mongoose.model('ImageMetaData');
 module.exports.storeImageMetaData = function(request, response) {
   var imageMetaData = new ImageMetaData({url: request.body.url, format: request.body.content_type});
   
-  console.log(imageMetaData);
+  imageMetaData.save();
+
+  // ImageMetaData.find({}, function(error, data) {
+  //   console.log(data);
+  //   res.json('data',data);
+  // });
+
   response.send(200, 'success');
 };
+
+  
