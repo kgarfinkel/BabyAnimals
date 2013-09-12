10d8770d4ecaea111c8162503b898008ac827720
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-11 19:28:31 -0700

    move post response to routes to make more modular

--- Dev/app/controllers/ImageData.js
+++ Dev/app/controllers/ImageData.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var ImageMetaData = mongoose.model('ImageMetaData');
 
 module.exports.storeImageMetaData = function(request, response) {
+  //store metadata to db
   var imageMetaData = new ImageMetaData({url: request.body.url, format: request.body.content_type}); 
   imageMetaData.save();
 
-  response.writeHead(200, {'Content-Type': 'application/json'});
-  response.end(JSON.stringify(imageMetaData));
+  return imageMetaData;
 };
 
   
--- Dev/config/routes.js
+++ Dev/config/routes.js
@@ -1,11 +1,20 @@
 module.exports = function(app) {
   //home route
-  var home = require('../app/controllers/home');
-  app.get('/', home.index);
+  // var home = require('../app/controllers/home');
+  // app.get('/', home.index);
 
   //image route  
   var imageDataController = require('../app/controllers/ImageData.js');
-  app.post('/image', imageDataController.storeImageMetaData);
+  app.post('/image', function(request, response) {
+
+    //store metadata to db 
+    var result = imageDataController.storeImageMetaData(request, response);
+
+    //respond with 200
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(result));
+
+  });
 
   // load url to server
   // load path
