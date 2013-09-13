292500408e14e364602fe15f0bc4c5d9e95a78e8
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-13 14:54:07 -0700

    export upload functions to lib for modularity

--- Dev/app/controllers/ImageData.js
+++ Dev/app/controllers/ImageData.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
-//var accessKey = require('./config/config').accessKey;
 var ImageMetaData = mongoose.model('ImageMetaData');
 
 module.exports.storeImageMetaData = function(features) {
+  
   //store metadata to db
   var imageMetaData = new ImageMetaData({
-    id: features.signatures,
+    id: features.signature,
     format: features.format,
     height: features.height,
-    width: features.width
+    width: features.width,
   });
 
   imageMetaData.save();
--- Dev/config/routes.js
+++ Dev/config/routes.js
@@ -1,35 +1,4 @@
-var request = require('request');
-var fs = require('fs');
-var url = require('url');
-var im = require('imagemagick');
-var imageDataController = require('../app/controllers/ImageData.js');
-
-//load file data to server
-var fetchURL = function(url, path, cb) {
-  var picStream = fs.createWriteStream(path);
-  picStream.on('close', function() {
-    cb();
-  });
-  request(url).pipe(picStream);
-
-};
-
-//store metadata to db 
-var addMetaData = function(path, cb) {
-
-  //add s3 url
-  im.identify('/Users/Kristina/js_books/temp/images', function(error, features) {
-    if (error) {
-      throw new Error('</3', error);
-    }
-
-    console.log('features', features);
-    //imageDataController.storeImageMetaData(features);
-  });
-
-  cb();
-
-};
+var upload = require('../lib/upload.js');
 
 module.exports = function(app) {
   //home route
@@ -37,14 +6,13 @@ module.exports = function(app) {
   app.get('/', home.index);
   
   //image route  
-  app.post('/image', function(req, response) {
+  app.post('/photo', function(req, response) {
 
     //add env[var]
     path = '/Users/Kristina/js_books/temp/images';
 
-    fetchURL(req.body.imgUrl, path, function() {
-      addMetaData(path, function() {
-        console.log('response');
+    upload.fetchURL(req.body.imgUrl, path, function() {
+      upload.addMetaData(path, function() {
         response.writeHead(200);
         response.end();
       });
