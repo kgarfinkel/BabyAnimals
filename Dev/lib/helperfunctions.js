8ee173bd37b0a8d21f20af6d9c6314323eecc60e
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-17 09:21:26 -0700

    require knox for helpers

--- Dev/lib/helperfunctions.js
+++ Dev/lib/helperfunctions.js
@@ -1,15 +1,23 @@
+var knox = require('knox');
+
 module.exports = {
   write: function(req, res, statusCode, body) {
     res.writeHead(statusCode, responseHeaders);
     res.end(body);
+  },
+
+  awsClient: function() {
+    return knox.createClient({
+      key: process.env.AWS_ACCESS_KEY,
+      secret: process.env.AWS_SECRET_KEY,
+      bucket: process.env.AWS_BUCKET
+    });  
   }
 };
 
-var responseHeaders = function() {
-  return {
-    "access-control-allow-origin": "*",
-    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "access-control-allow-headers": "content-type, accept",
-    "access-control-max-age": 10 // Seconds.
-  };
+var responseHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10
 };
