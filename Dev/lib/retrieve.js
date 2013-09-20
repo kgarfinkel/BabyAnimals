b975156eb0d591503666e76e4f25ed55dcd1f49d
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-20 10:00:55 -0700

    fix syntax error in fs.exists check

--- Dev/lib/retrieve.js
+++ Dev/lib/retrieve.js
@@ -10,36 +10,37 @@ module.exports = {
   retrieve: function(req, res, next) {
       if (req.params.image) {
         //if file exists locally continue onto next middleware 
-        if (fs.exists(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg')) {
-          next();
+        fs.exists(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg', function(exists) {
+          if (exists) {
+            next();
+          }
+        });
 
-        } else {
-          //stream to local fs
-          var outstream = fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg');
-          
-          //store in s3
-          var s3req = client.get(req.key);
+        //stream to local fs
+        var outstream = fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg');
+        
+        //store in s3
+        var s3req = client.get(req.key);
 
-          s3req.on('response', function(res) {
-            res.on('data', function(chunk) {
-              outstream.write(chunk);
-            });
-
-            res.on('error', function(err) {
-              console.log(err);
-            });
-
-            //when response from s3 has ended
-            //route to next middleware (if applicable)
-            res.on('end', function() {
-              next();
-            });
+        s3req.on('response', function(res) {
+          res.on('data', function(chunk) {
+            outstream.write(chunk);
           });
 
-          //end s3 request
-          s3req.end();
+          res.on('error', function(err) {
+            console.log(err);
+          });
 
-        }
+          //when response from s3 has ended
+          //route to next middleware (if applicable)
+          res.on('end', function() {
+            next();
+          });
+        });
+
+        //end s3 request
+        s3req.end();
+
       } 
     }
 };
