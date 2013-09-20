2db450b675d15a7396a63c75139931932392ae5f
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-19 23:18:06 -0700

    add filters

--- Dev/lib/filters.js
+++ Dev/lib/filters.js
@@ -1,11 +1,15 @@
 //dependencies
 var helpers = require('./helperfunctions');
+var filterHelpers = require('./filterhelpers.js');
 var upload = require('./upload');
+var resize = require('./resize');
 var fs = require('fs');
 var gm = require('gm');
+var im = require('imagemagick');
 var uuid = require('node-uuid');
 var knox = require('knox');
-var imageDataController = require('../app/controllers/ImageData.js');
+var spawn = require('child_process').spawn;
+var Q = require('q');
 
 module.exports = {
   //route filter based on filter param
@@ -75,11 +79,11 @@ var filters = {
     var key = uuid.v4().split('-').pop();
 
     gm(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg')
-    .modulate(150, 70, 100)
+    .modulate(150, 80, 80)
     .gamma(1.2)
-    .contrast(+2)
-    .fill(330000)
-    .colorize()
+    .fill('#330000')
+    .colorize(40)
+    .contrast(+1)
     .write(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', function(err) {
       if (err) {
         throw err;
@@ -125,7 +129,7 @@ var filters = {
     gm(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg')
     .fill('#222b6d')
     .colorize(30)
-    .modulate(70, 80, 100)
+    .modulate(90, 80, 100)
     .compose('Over')
     .write(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', function(err) {
       if (err) {
@@ -136,47 +140,115 @@ var filters = {
     });
   },
 
-  fade: function(req, res) {
-      console.log('red');
-      var key = uuid.v4().split('-').pop();
+  gotham: function(req, res) {
+    var key = uuid.v4().split('-').pop();
 
-      gm(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg')
-      .fill('#222b6d')
-      .colorize(30)
-      .fill('#f7daae')
-      .colorize(30)
-      .modulate(10, 50, 110)
-      .contrast()
-      .contrast()
-      .compose('Over')
-      .write(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', function(err) {
+    gm(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg')
+    .modulate(120, 10, 100)
+    .fill('#222b6d')
+    .colorize(20)
+    .gamma(0.5)
+    .contrast()
+    .contrast()
+    .compose('Over')
+    .borderColor('black')
+    .border(7,7)
+    .write(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', function(err) {
+      if (err) {
+        throw err;
+      }
+
+      helpers.helper.upload(req, res, process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', 'transform', key );
+    });
+  },
+
+  //brighten
+
+  bw_grad: function(req, res)  {
+    var w;
+    var h;
+    var key = uuid.v4().split('-').pop();
+    
+    //obtain the dimensions of the requested image
+    im.convert([process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg', '-colorspace', 'gray', process.env.LOCAL_FILE_PATH + 'temp_' + req.key + '.jpg' ], 
+    function(err, stdout){
+      if (err) {
+        throw err;
+      }
+    
+      im.identify(process.env.LOCAL_FILE_PATH + '/temp_' + req.key + '.jpg', function(err, features) {
         if (err) {
+          console.error('could not process image </3');
           throw err;
         }
 
-        helpers.helper.upload(req, res, process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', 'transform', key );
-      });
-    },
+        w = features.width;
+        h = features.height;
 
-    gotham: function(req, res) {
-      var key = uuid.v4().split('-').pop();
-
-      gm(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg')
-      .modulate(120, 10, 100)
-      .fill('#222b6d')
-      .colorize(20)
-      .gamma(0.5)
-      .contrast()
-      .contrast()
-      .compose('Over')
-      .borderColor('black')
-      .border(7,7)
-      .write(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', function(err) {
-        if (err) {
+        Q.fcall(resize(req, res, w, h, key))
+        .then(bw_grad2(req, res, key))
+        .then(helpers.helper.upload(req, res, process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', 'transform', key ))
+        .catch(function(err) {
           throw err;
-        }
+        });
 
-        helpers.helper.upload(req, res, process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', 'transform', key );
       });
-    }
+    });
+  },
+
+  vintage: function(req, res) {
+    var w;
+    var h;
+    var key = uuid.v4().split('-').pop();
+
+    im.identify(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg', function(err, features) {
+      if (err) {
+        console.error('could not process image </3');
+        throw err;
+      }
+
+      w = features.width;
+      h = features.height;
+
+
+      resize(req, res, w, h, key);
+    });
+  }
 };
+
+
+var resize =  function(req, res, w, h, key) {
+  im.resize({
+    srcPath: '/Users/Kristina/Desktop/antiquep.jpg',
+    dstPath: '/Users/Kristina/Desktop/tempantique.jpg',
+    width: w,
+    height: h
+  }, function(err, stdout, stderr) {
+    if (err) {
+      console.error('could not process image </3');
+      throw err;
+    }
+
+    if (stderr) {
+      throw stderr;
+    }
+  });
+
+  addHipsterOverlay(res, req, key);
+
+};
+
+var addHipsterOverlay = function(res, req, key) {
+  var args = [process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg', '/Users/Kristina/Desktop/tempantique.jpg', '-compose', 'Overlay', '-composite', process.env.LOCAL_FILE_PATH + '/' + key + '.jpg'];
+  var convert = spawn('convert', args);
+
+  convert.stdout.pipe(fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg'));
+};
+
+// var bw_grad2 = function(res, req, key) {
+//   console.log('key', key);
+//   var args = ['-watermark', '90%', '-gravity', 'center', process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg' , '/Users/Kristina/Desktop/temp_bwgrad_1.jpg', '-'];
+//   var composite = spawn('composite', args);
+    
+//   composite.stdout.pipe(fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg'));
+// };
