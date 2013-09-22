2e8461684440c1124360a0b8f7eb872a29985623
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-22 14:09:45 -0700

    add cron job

--- Dev/lib/cronjob.js
+++ Dev/lib/cronjob.js
@@ -1,26 +1,19 @@
 //dependencies
 var mongoose = require('mongoose');
 var cronJob = require('cron').CronJob;
+var removefileforce = require('./removefileforce');
 
 module.exports = function(app, config) {
-  var job = new cronJob('0 0 * * 0', function() {
-    //remove all documents from imatemetadatas
-    mongoose.connection.db.collection('imagemetadatas', function(err, collection) {
-      if (err) {
-        console.error('</3');
-        throw err;
-      }
-
-      collection.remove({}, function(err, removed) {
-        if (err) {
-          console.error('</3');
-          throw err;
-        }
-      });
-    });
+  consoe.log('in corn job');
+  var job = new cronJob('* * * * *', function() {
+    console.log('cron job started');
+    //remove all files from image folder
+    var rootPath = __dirname + './../data/images';
+    removefileforce(rootPath);
   }, function() {
     console.log('cron job stopped');
   });
 
   job.start();
 };
+
