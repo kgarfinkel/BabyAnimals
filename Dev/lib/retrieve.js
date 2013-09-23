a4833280365120749bb9692b903d4a075f4b46e0
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-23 12:01:09 -0700

    check if local file exists

--- Dev/lib/retrieve.js
+++ Dev/lib/retrieve.js
@@ -8,37 +8,36 @@ module.exports = {
     if (req.params.image) {
       //if file exists locally continue onto next middleware 
       //TODO: why isn't this working
-      // fs.exists(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg', function(exists) {
-      //   if (exists) {
-      //     next();
-      //   }
-      // });
-
-      //stream to local fs
-      var outstream = fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg');
-      //store in s3
-      var s3req = client.get(req.key);
-
-      s3req.on('response', function(res) {
-        res.on('data', function(chunk) {
-          outstream.write(chunk);
-        });
-
-        res.on('error', function(err) {
-          console.error('</3');
-          throw err;
-        });
-
-        //when response from s3 has ended
-        //route to next middleware (if applicable)
-        res.on('end', function() {
+      fs.exists(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg', function(exists) {
+        if (exists) {
           next();
-        });
+        } else {
+          //stream to local fs
+          var outstream = fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + req.key + '.jpg');
+          //store in s3
+          var s3req = client.get(req.key);
+
+          s3req.on('response', function(res) {
+            res.on('data', function(chunk) {
+              outstream.write(chunk);
+            });
+
+            res.on('error', function(err) {
+              console.error('</3');
+              throw err;
+            });
+
+            //when response from s3 has ended
+            //route to next middleware (if applicable)
+            res.on('end', function() {
+              next();
+            });
+          });
+
+          //end s3 request
+          s3req.end();
+        }
       });
-
-      //end s3 request
-      s3req.end();
-
     } 
   }
 };
