2c12fb072e86a044c27e54250b5bfef028cb21a9
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-22 19:48:45 -0700

    upload local fs and urls

--- Dev/lib/upload.js
+++ Dev/lib/upload.js
@@ -12,13 +12,37 @@ module.exports = {
   //stream requested image to fs and store in s3 bucket
   upload: function(req, res) {
     var key = uuid.v4().split('-').pop();
-    var outStream = fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg');
 
-    request(req.query.imgUrl).pipe(outStream);
+    if (req.query.src.indexOf('http') !== -1) {
+      var outStream = fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg');
+      request(req.query.src).pipe(outStream);
 
-    //TODO: use graphics magick here?
-    outStream.on('close', function() {
-      fs.readFile(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', function(err, buff){
+      //TODO: use graphics magick here?
+      outStream.on('close', function() {
+        fs.readFile(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg', function(err, buff) {
+          var req = client.put(key, {
+              'Content-Length': buff.length,
+              'Content-Type': 'image/jpeg',
+              'x-amz-acl': 'public-read'
+            });
+
+          req.on('response', function(resp){
+            if (resp.statusCode === 200) {
+              helpers.addToDb(req, res, key);
+              helpers.getDimensions(req, res, key, helpers.response);
+            }
+          });
+
+          //TODO: response status code if s3 status is not too
+          req.end(buff);
+        });
+      });
+    } else {
+      var readStream = fs.createReadStream(req.query.src);
+      var writeStream = fs.createWriteStream(process.env.LOCAL_FILE_PATH + '/' + key + '.jpg');
+
+      readStream.on('data', function(buff) {
+
         var req = client.put(key, {
             'Content-Length': buff.length,
             'Content-Type': 'image/jpeg',
@@ -34,8 +58,19 @@ module.exports = {
 
         //TODO: response status code if s3 status is not too
         req.end(buff);
-      });
-    });
+        
+        console.log('buff', buff);
 
+        writeStream.write(buff);
+      });
+
+      readStream.on('end', function() {
+        writeStream.end();
+      });
+
+      writeStream.on('close', function() {
+        console.log('close');
+      });
+    }
   }
 };
