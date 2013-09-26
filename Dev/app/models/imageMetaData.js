2729d3f7f93724703425abdba402361677e58f35
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-25 22:56:10 -0700

    deleted files

--- Dev/app/models/imageMetaData.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var imageMetaSchema = new Schema({
-  key: String,
-});
-
-module.exports = mongoose.model('ImageMetaData', imageMetaSchema);
--- Dev/lib/mongoosehelpers.js
+++
@@ -1,8 +0,0 @@
-var imageData = require('../app/controllers/ImageData.js');
-
-//store s3 key in db 
-module.exports = {
-  addToDb: function(req, res, key) {
-    var imgKey = imageData.imageData(key);
-  }
-};
