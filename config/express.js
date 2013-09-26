6f37429fde602f3ffcc1f00b508d1e1df7024913
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-26 14:19:32 -0700

    add errorhandler middleware

--- config/express.js
+++ config/express.js
@@ -7,11 +7,12 @@ module.exports = function(app, config) {
     app.set('port', config.port);
     app.set('views', config.root + '/app/views');
     app.set('view engine', 'jade');
-    // app.use(express.favicon(config.root + '/public/img/favicon.ico'));
+    app.use(express.favicon(config.root + '/public/img/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
+    app.use(express.errorHandler({showStack: true, dumpExceptions: true}));
     app.use(function(req, res) {
       res.status(404).render('404', { title: '404' });
     });
