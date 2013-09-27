61874f3d7a44dbda0331d60c802cca7e2fef94d0
Kristina Brainwave <kristina.garfinkel@gmail.com>
2013-09-26 18:49:40 -0700

    add babyanimals as route prefix

--- config/routes.js
+++ config/routes.js
@@ -14,12 +14,12 @@ module.exports = {
   routeHandler: function(app) {    
     //middleware for any image retrieval
     app.param('image', function(req, res, next, image) {
-      model.find({key:image}, function(error, data) {
+      model.findOne({key:image}, function(error, data) {
         if (error) {
           return next(error);
         }
 
-        if (data.length === 0) {
+        if (!data) {
           console.error('image has not been uploaded </3');
           return response.errRes(req, res);
         }
@@ -40,28 +40,29 @@ module.exports = {
     app.get('/', home.index);
 
     //upload route
-    app.post('/upload', upload.upload, function(req, res, next) {
+    app.post('/babyanimals/upload', upload.upload, function(req, res, next) {
     });
 
     //get image route
-    app.get('/:image', retrieve.retrieve, function(req, res, next) {
+    app.get('/babyanimals/:image', retrieve.retrieve, function(req, res, next) {
       response.getRes(req, res, req.key);
     });
 
     //resize image route
-    app.get('/:image/size', retrieve.retrieve, resize.identify, function(req, res, next) {
+    app.get('/babyanimals/:image/size', retrieve.retrieve, resize.identify, function(req, res, next) {
     });
 
     //delete image route
-    app.get('/:image/del', del.del, function(req, res, next) {  
+    app.get('/babyanimals/:image/del', del.del, function(req, res, next) {  
     });
 
+    //TODO: make
     //get image info route
-    app.get('/:image/info', retrieve.retrieve, helpers.getDimensions, function(req, res, next) {
+    app.get('/babyanimals/:image/info', retrieve.retrieve, helpers.getDimensions, function(req, res, next) {
     });
 
     //transform image
-    app.get('/:image/:filter', retrieve.retrieve, filters.routeFilter, function(req, res, next) {
+    app.get('/babyanimals/:image/:filter', retrieve.retrieve, filters.routeFilter, function(req, res, next) {
     });
   }
 };
